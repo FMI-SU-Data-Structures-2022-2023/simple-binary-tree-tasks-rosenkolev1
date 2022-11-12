3cadcca194bca74004857b77983dc914f84bee28
rosenkolev1 <50500415+rosenkolev1@users.noreply.github.com>
2022-11-12 19:09:39 +0200

    Add some solutions to SDP Tasks for Binary trees

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,27 +1,52 @@
 #include "solution.h"
+#include <iostream>
 
 int countElements(Node* tree) {
-    return -1;
+
+    int count = 0;
+    if (tree == nullptr) return 0;
+
+    count = 1 + countElements(tree->left) + countElements(tree->right);
+    return count;
+
 }
 
 int countLeafs(Node* tree) {
-    return -1;
+    
+    if (tree == nullptr) return 0;
+    if (tree->left == nullptr && tree->right == nullptr) return 1;
+    
+    return countLeafs(tree->left) + countLeafs(tree->right);
 }
 
 int height(Node* tree) {
-    return -1;
+    
+    if (tree == nullptr) return 0;
+
+    return 1 + (std::max(height(tree->left), height(tree->right)));
+
 }
 
 int sumElements(Node* tree) {
-    return -1;
+    if (tree == nullptr) return 0;
+
+    return tree->key + sumElements(tree->left) + sumElements(tree->right);
 }
 
 int sumElementsAtLevel(Node* tree, unsigned level) {
-    return -1;
+
+    if (tree == nullptr) return 0;
+    
+    if (level == 0)
+    {
+        return tree->key;
+    }
+
+    return sumElementsAtLevel(tree->left, level - 1) + sumElementsAtLevel(tree->right, level - 1);
 }
 
 bool isSymmetric(Node* tree) {
-    return false;
+    
 }
 
 bool isBST(Node* tree) {
